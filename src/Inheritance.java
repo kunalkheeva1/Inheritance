da1179aea29dcc71c18bc10be8bccb0ba5e77f92
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-03-27 22:13:49 -0700

    Initial commit

--- src/Inheritance.java
+++ src/Inheritance.java
@@ -1,15 +1,27 @@
-interface Kunal{
-    default void kk(){
+interface Kunal {
+    default void kk() {
         System.out.println("kheeva");
     }
-    interface Kheeva{
+}   interface Kheeva{
+        default void kk(){
+            System.out.println("paji");
 
+        }
+
+    }
+class Inheritance implements Kunal, Kheeva{
+    public void kk(){
+        Kunal.super.kk();
+        Kheeva.super.kk();
+    }
+
+    public static void main(String[] args) {
+        Inheritance nn= new Inheritance();
+        nn.kk();
     }
 }
 
 
 
 
-public class Inheritance {
 
-}
